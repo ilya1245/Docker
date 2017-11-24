190d79383201531a05c53d8aadff13d6dce705ae
ilya1245 <ilya1245@gmail.com>
2017-11-25 01:49:23 +0200

    tomcat-ssh - ssh autorun

--- docker-builds/alpine/tomcat-ssh/run.sh
+++ docker-builds/alpine/tomcat-ssh/run.sh
@@ -1,3 +1,4 @@
 # docker run -d --name my_tomee -p 8080:8080 sgrio/tomee
 # docker run -d --name my_tomcat_ssh -p 122:22 --env ROOT_PASSWORD=280373 imiflig/tomcat-ssh /opt/tomcat/bin/catalina.sh run
 docker run -d --name my_tomcat_ssh -p 122:22 -p 18080:8080 -p 18443:8443 --env ROOT_PASSWORD=280373 imiflig/tomcat-ssh
+docker exec -d -it my_tomcat_ssh /usr/local/bin/entrypoint.sh
