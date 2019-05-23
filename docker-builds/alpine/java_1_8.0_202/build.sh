58ac251f70057096db32d7c3de11b38dbaa91140
Ilya Miflig <Ilya_Miflig@epam.com>
2019-05-23 12:26:25 +0300

    Added jdk 1.8.202 image

---
+++ docker-builds/alpine/java_1_8.0_202/build.sh
@@ -0,0 +1,1 @@
+docker build -t imiflig/alpine-java .
---
+++ docker-builds/alpine/java_1_8.0_202/run.sh
@@ -0,0 +1,2 @@
+# works # docker run -it --name c_wdg -p 28080:8080 wdg java -jar weatherDataGatherer-0.1.0.jar
+docker run -d --name weather_dg -p 8090:8090 imiflig/alpine-java java -jar /home/apps/spring_boot/weatherDataGatherer-0.1.0.jar
