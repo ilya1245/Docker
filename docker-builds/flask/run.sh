64d05575002cee21a0b126fc6305eab8704afe75
ilya1245 <ilya1245@gmail.com>
2019-12-26 12:02:21 +0200

    Flask - run sample project

--- docker-builds/flask/run.sh
+++ docker-builds/flask/run.sh
@@ -1,1 +1,2 @@
-docker run -v /home/ilya/docker_share/flask/app:/app/app -d --name=my_flask -p 5000:5000 -e PORT="5000" imiflig/flask
+# Execute /app/app/sample_app.py using -e MODULE_NAME="app.sample_app"
+docker run -v /home/ilya/docker_share/flask/app:/app/app -d --name=my_flask1 -p 5000:5000 -e PORT="5000" -e MODULE_NAME="app.sample_app" imiflig/flask
