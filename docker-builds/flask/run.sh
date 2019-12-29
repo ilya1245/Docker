bad4f724b4a5a30ebab323ab601af88a62eb31df
ilya1245 <ilya1245@gmail.com>
2019-12-30 00:33:27 +0200

    Reworked flask image

--- docker-builds/flask/run.sh
+++ docker-builds/flask/run.sh
@@ -1,2 +1,2 @@
 # Execute /app/app/sample_app.py using -e MODULE_NAME="app.sample_app"
-docker run -v /home/ilya/docker_share/flask/app:/app/app -d --name=my_flask1 -p 5000:5000 -e PORT="5000" -e MODULE_NAME="app.sample_app" imiflig/flask
+docker run -v /home/ilya/docker_share/flask/app:/app/app -d --name=my_flask -p 5000:5000 -e PORT="5000" -e MODULE_NAME="app.predict_app" imiflig/flask
